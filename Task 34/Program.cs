8c5122a227bf66c76e97c5be6ca8fead1a19887a
Artem <fedchikhin@yandex.ru>
2023-04-06 14:17:41 +0300

    добавил дз

---
+++ Task 34/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+
+void InputArray(int[] array) 
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100,  1000);
+}
+
+int ReleaseArray(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+        if (array[i] %2 == 0)
+            count++;
+        Console.Write(count);
+    return  count;                  
+    
+}
+
+void PrintArray(int[] array) 
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+
+Console.Clear();
+Console.WriteLine($"n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+PrintArray(array);
+ReleaseArray(array);
---
+++ Task 36/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+//  Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+void InputArray(int[] array) 
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-25,  26);
+}
+
+int ReleaseArray(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i+=2)
+        count = count + array[i];
+        Console.Write(count);
+        return count;
+}
+
+void PrintArray(int[] array) 
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+
+Console.Clear();
+Console.WriteLine($"n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+PrintArray(array);
+ReleaseArray(array);
+
+
+
+
+
+
+
+
+
+
